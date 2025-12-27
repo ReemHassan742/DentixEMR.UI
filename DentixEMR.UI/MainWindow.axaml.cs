b1bc44ff589eeac0a4c8d149095cc74686b6ac6f
Reem Hassan <reemhassan742r@gmail.com>
2025-12-27 03:20:40 +0200

    Add project files.

---
+++ DentixEMR.UI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DentixEMR.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
